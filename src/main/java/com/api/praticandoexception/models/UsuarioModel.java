dd9fc820780ad8a9e9dfcd4284e007ce2da59d34
claudiovdev <c.viniciusmagalhaes@gmail.com>
2022-12-01 20:40:37 -0300

    Criando as anotações dentro de usuario

--- src/main/java/com/api/praticandoexception/models/UsuarioModel.java
+++ src/main/java/com/api/praticandoexception/models/UsuarioModel.java
@@ -1,9 +1,41 @@
 package com.api.praticandoexception.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@EqualsAndHashCode
+@Entity
+@Table(name = "TB_USUARIOS")
 public class UsuarioModel {
 
+    @EqualsAndHashCode.Exclude
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String id;
+
+    @Column(length = 100, nullable = false)
     private String nome;
+
+    @Column(length = 50, nullable = false, unique = true)
     private String email;
+
+    @Column(length = 20)
+    private String numeroTelefone;
+
+    @CreationTimestamp
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private LocalDateTime dataDeCadastro;
+
+    @UpdateTimestamp
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private LocalDateTime dataDeAtualizacao;
 
 }
