88e375a166b83ff841b827aba6ab1d10fb5c57c3
claudiovdev <c.viniciusmagalhaes@gmail.com>
2022-12-01 20:22:07 -0300

    Adicionando projetos com pacotes

---
+++ src/main/java/com/api/praticandoexception/PraticandoExceptionApplication.java
@@ -0,0 +1,13 @@
+package com.api.praticandoexception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticandoExceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticandoExceptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/praticandoexception/PraticandoExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.praticandoexception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticandoExceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
