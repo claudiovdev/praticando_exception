b1218b7daf97d5f0fafd78ac8d028ecabf4ae8ab
claudiovdev <c.viniciusmagalhaes@gmail.com>
2022-12-01 22:30:58 -0300

    Adicionando todos os usuarios

--- src/main/java/com/api/praticandoexception/services/UsuarioService.java
+++ src/main/java/com/api/praticandoexception/services/UsuarioService.java
@@ -2,6 +2,10 @@ package com.api.praticandoexception.services;
 
 import com.api.praticandoexception.models.UsuarioModel;
 
+import java.util.List;
+
 public interface UsuarioService {
     UsuarioModel salvar(UsuarioModel usuario);
+
+    List<UsuarioModel> listarUsuarios();
 }
--- src/main/java/com/api/praticandoexception/services/impl/UsuarioServiceImpl.java
+++ src/main/java/com/api/praticandoexception/services/impl/UsuarioServiceImpl.java
@@ -7,6 +7,7 @@ import com.api.praticandoexception.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -21,6 +22,11 @@ public class UsuarioServiceImpl implements UsuarioService {
         usuario.setStatus(StatusDoUsuario.ATIVO);
         usuario = emailMinusculo(usuario);
         return repository.save(usuario);
+    }
+
+    @Override
+    public List<UsuarioModel> listarUsuarios() {
+        return repository.findAll();
     }
 
     public UsuarioModel emailMinusculo(UsuarioModel usuario){
